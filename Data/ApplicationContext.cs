e87fff6fc072188faed1840f6f64628538f6f834
Daniel Martins <freitasdaniel39@gmail.com>
2023-04-28 20:07:19 -0300

    creating entities

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DominandoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
